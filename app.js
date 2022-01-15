0a1abe9fda75d484a2235053456a907bcc2036fc
Mayank Dahiya <dahiyamayank0@gmail.com>
2022-01-15 22:42:02 +0530

    One Line Added

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const app = express();
 app.use(require("./routers/auth"));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-const PORT = process.env.PORT;
-app.listen(PORT, function(err){
+app.listen(process.env.PORT || 8080, function(err){
     console.log("Server Started on 3000");
 });
